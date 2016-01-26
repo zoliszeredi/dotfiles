126b8d25a258709358e80470d86d42f6474b155e
Zoltán T. Szeredi <zoltan.szeredi@3pillarglobal.com>
2016-01-26 09:54:16 +0200

    Add default font face in .emacs.el.

--- .emacs.el
+++ .emacs.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(column-number-mode t)
- '(custom-enabled-themes (quote (whiteboard)))
+ '(custom-enabled-themes (quote (wombat)))
  '(gud-gdb-command-name "gdb --annotate=1")
  '(inferior-js-program-command "nodejs --interactive")
  '(inhibit-startup-screen t)
@@ -37,7 +37,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Fira Mono" :foundry "unknown" :slant normal :weight normal :height 128 :width normal)))))
 
 (require 'package)
 (package-initialize)
