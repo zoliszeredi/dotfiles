17fd9b0494a04e87d92447caa027038e1a2ad5fc
Tibor Zoltán Szeredi <zoli.szeredi@gmail.com>
2019-06-12 15:23:30 +0300

    Update configs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,36 +3,38 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(column-number-mode t)
- ;; '(custom-enabled-themes (quote (wombat)))
- '(gud-gdb-command-name "gdb --annotate=1")
- '(inferior-js-program-command "nodejs --interactive")
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(display-time-24hr-format t)
+ '(display-time-day-and-date t)
  '(inhibit-startup-screen t)
  '(large-file-warning-threshold nil)
- ;; '(flycheck-python-pylint-executable "/home/stz/.envs/pylint/bin/pylint")
  '(menu-bar-mode nil nil (menu-bar))
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa-stable" . "http://stable.melpa.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (pomidor python python-pytest org magit flycheck docker-compose-mode docker)))
  '(safe-local-variable-values
-   (quote ((eval when (string-match "\\.h\\'" (buffer-file-name))
-		 (unless (string-match "/gnulib/" (buffer-file-name))
-		   (c++-mode) (c-set-style "gnu"))))))
+   (quote
+    ((eval when
+	   (string-match "\\.h\\'"
+			 (buffer-file-name))
+	   (unless
+	       (string-match "/gnulib/"
+			     (buffer-file-name))
+	     (c++-mode)
+	     (c-set-style "gnu"))))))
  '(save-place t nil (saveplace))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
+ '(show-trailing-whitespace t)
  '(tool-bar-mode nil)
  '(tooltip-mode nil)
- '(show-trailing-whitespace t)
- '(package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;		      ("marmalade" . "http://marmalade-repo.org/packages/")
-		      ("melpa-stable" . "http://stable.melpa.org/packages/")))
- '(scheme-program-name "guile")
- '(python-shell-interpreter "python")
- '(uniquify-buffer-name-style 'post-forward)
- '(uniquify-separator ":")
- '(display-time-day-and-date t)
- '(display-time-24hr-format t)
-)
-
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
+ '(uniquify-separator ":"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -43,36 +45,40 @@
 
 (require 'package)
 (package-initialize)
-(package-refresh-contents)
 
 (require 'uniquify)
 (require 'ido)
-
-(package-install 'flycheck)
-(package-install 'magit)
-(package-install 'cider)
-(package-install 'slime)
-(package-install 'geiser)
-(package-install 'jsx-mode)
-(package-install 'lua-mode)
 
 (put 'dired-find-alternate-file 'disabled nil)
 (ido-mode t)
 (global-flycheck-mode)
 
 (defun my-dired-mode-setup ()
-  "Hide the silly extra columns in dired view"
+  "Hide the silly extra columns in dired view."
   (dired-hide-details-mode 1))
+
+(defun run-tox ()
+  "Run tox async shell."
+  (async-shell-command "tox"))
 
 (add-hook 'dired-mode-hook 'my-dired-mode-setup)
 (global-set-key (kbd "C-?") 'delete-backward-char)
-
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
-(autoload 'jsx-mode "jsx-mode" "JSX mode" t)
-
-
-(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
-(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+(global-set-key (kbd "C-M-t") 'run-tox)
 
 (display-time)
+
+
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+	(backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
